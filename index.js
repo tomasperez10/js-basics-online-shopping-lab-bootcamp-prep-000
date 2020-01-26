e8f30e5122c271fb80dbbb78cf5beb12eed046e4
Tomas Perez <tomasitopg6@gmail.com>
2020-01-26 23:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ function placeOrder(cardNumber) {
       
       return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`
     }
-    return 'Sorry, we don\'t have a credit card on file for you.'
   }
-  
+   return 'Sorry, we don\'t have a credit card on file for you.'
 }
 
