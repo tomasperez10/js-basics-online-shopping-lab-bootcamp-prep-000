64a421c4d036768314d67dce4f3485947a43efd1
Tomas Perez <tomasitopg6@gmail.com>
2020-01-26 23:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 
 function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++){
-    if(cart[i].itemName === 2){
+    if(cart[i].itemName === item){
       return cart.pop(cart[i].itemName)
     }
     return 'That item is not in your cart.'
