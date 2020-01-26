09d2c4a7ac577976aec7fc057f0251140a31bcdf
Tomas Perez <tomasitopg6@gmail.com>
2020-01-26 23:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,9 +55,8 @@ function removeFromCart(item) {
       
       return cart
     }
-    return 'That item is not in your cart.'
   }
-  
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
